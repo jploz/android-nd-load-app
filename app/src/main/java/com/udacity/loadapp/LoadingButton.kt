1bb67b6fc84e91d45b22ade2630bf97d83603995
Jean-Peer Lorenz <peer.loz@gmx.net>
2021-03-05 07:18:48 +0100

    Adjust java package name to com.udacity.loadapp

--- app/src/main/java/com/udacity/loadapp/LoadingButton.kt
+++ app/src/main/java/com/udacity/loadapp/LoadingButton.kt
@@ -1,4 +1,4 @@
-package com.udacity
+package com.udacity.loadapp
 
 import android.animation.ValueAnimator
 import android.content.Context
--- app/src/test/java/com/udacity/loadapp/ExampleUnitTest.kt
+++ app/src/test/java/com/udacity/loadapp/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.udacity
+package com.udacity.loadapp
 
 import org.junit.Test
 
