08170d7fcfc3c306a5fc38546108d0d0eb3d00cd
Jean-Peer Lorenz <peer.loz@gmx.net>
2021-03-09 06:11:42 +0100

    Refactor: move download function from main activity into view model

--- app/src/main/java/com/udacity/loadapp/view/main/MainActivity.kt
+++ app/src/main/java/com/udacity/loadapp/view/main/MainActivity.kt
@@ -21,13 +21,12 @@ import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var downloadID: Long = 0
-
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,14 +35,14 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
-        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-            download()
+            viewModel.download()
         }
     }
 
@@ -51,26 +50,6 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
         }
-    }
-
-    private fun download() {
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
-
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
-    }
-
-    companion object {
-        private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
-        private const val CHANNEL_ID = "channelId"
     }
 
 }
--- app/src/main/java/com/udacity/loadapp/view/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/loadapp/view/main/MainViewModel.kt
@@ -1,8 +1,34 @@
 package com.udacity.loadapp.view.main
 
 import android.app.Application
+import android.app.DownloadManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.AndroidViewModel
+import com.udacity.loadapp.R
 
 class MainViewModel(private val app: Application) : AndroidViewModel(app) {
 
+    private var downloadID: Long = 0
+
+    companion object {
+        private const val URL =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val CHANNEL_ID = "channelId"
+    }
+
+    fun download() {
+        val request =
+            DownloadManager.Request(Uri.parse(URL))
+                .setTitle(app.getString(R.string.app_name))
+                .setDescription(app.getString(R.string.app_description))
+                .setRequiresCharging(false)
+                .setAllowedOverMetered(true)
+                .setAllowedOverRoaming(true)
+
+        val downloadManager =
+            app.getSystemService(AppCompatActivity.DOWNLOAD_SERVICE) as DownloadManager
+        downloadID =
+            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+    }
 }
